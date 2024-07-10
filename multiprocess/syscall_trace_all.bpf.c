42c070290c78383b3d8da267afbf85bff7a2c98d
PGHOON <park1578@purdue.edu>
2024-07-10 15:22:51 +0900

    [Feat] add syscall trace targeting all processes

--- multiprocess/syscall_trace_all.bpf.c
+++ multiprocess/syscall_trace_all.bpf.c
@@ -2,7 +2,7 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
-#include "dcars.h"
+#include "syscall_trace_all.h"
 
 struct {
     __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
