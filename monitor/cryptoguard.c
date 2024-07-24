5165d9c96629d5abf17a7c17d030dba90fb7f377
PGHOON <park1578@purdue.edu>
2024-07-24 09:21:30 +0900

    [Feat] csv --> sketch

--- monitor/cryptoguard.c
+++ monitor/cryptoguard.c
@@ -9,9 +9,16 @@
 #include "string.h"
 #include <limits.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #define COMMAND_LEN 16
 #define MAX_FILES 256
+
+#define W 272
+#define D 3
+#define HASH_SEED 1934
 
 /*
 TEST CODE
@@ -67,6 +74,107 @@ void handle_event(void *ctx, int cpu, void *data, unsigned int data_sz)
 }
 
 void lost_event(void *ctx, int cpu, long long unsigned int data_sz){}
+
+unsigned int hash(const char *str, int seed) {
+    unsigned long hash = seed;
+    int c;
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + c;
+    return hash % W;
+}
+
+typedef struct {
+    int table[D][W];
+} CountMinSketch;
+
+void initCMS(CountMinSketch *cms) {
+    for (int i = 0; i < D; i++) {
+        for (int j = 0; j < W; j++) {
+            cms->table[i][j] = 0;
+        }
+    }
+}
+
+void updateCMS(CountMinSketch *cms, const char *item) {
+    for (int i = 0; i < D; i++) {
+        int index = hash(item, HASH_SEED + i);
+        cms->table[i][index]++;
+    }
+}
+
+int queryCMS(CountMinSketch *cms, const char *item) {
+    int minCount = cms->table[0][hash(item, HASH_SEED)];
+    for (int i = 1; i < D; i++) {
+        int index = hash(item, HASH_SEED + i);
+        if (cms->table[i][index] < minCount) {
+            minCount = cms->table[i][index];
+        }
+    }
+    return minCount;
+}
+
+void printCMS(CountMinSketch *cms) {
+    printf("Count-Min Sketch Table:\n");
+    for (int i = 0; i < D; i++) {
+        for (int j = 0; j < W; j++) {
+            printf("%d ", cms->table[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void saveCMSToCSV(CountMinSketch *cms, const char *filename) {
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        perror("fopen");
+        return;
+    }
+
+    for (int i = 0; i < D; i++) {
+        for (int j = 0; j < W; j++) {
+            fprintf(file, "%d", cms->table[i][j]);
+            if (j < W - 1) {
+                fprintf(file, ",");
+            }
+        }
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
+}
+
+void processCSVFiles(CountMinSketch *cms, const char *directory) {
+    struct dirent *entry;
+    DIR *dp = opendir(directory);
+
+    if (dp == NULL) {
+        perror("opendir");
+        return;
+    }
+
+    while ((entry = readdir(dp))) {
+        if (entry->d_type == DT_REG) {
+            char filepath[256];
+            snprintf(filepath, sizeof(filepath), "%s/%s", directory, entry->d_name);
+            FILE *file = fopen(filepath, "r");
+            if (file == NULL) {
+                perror("fopen");
+                continue;
+            }
+
+            char line[1024];
+            while (fgets(line, sizeof(line), file)) {
+                char *system_call = strtok(line, "\n");
+                if (system_call != NULL) {
+                    updateCMS(cms, system_call);
+                }
+            }
+            fclose(file);
+        }
+    }
+
+    closedir(dp);
+}
 
 int main()
 {
@@ -145,6 +253,21 @@ int main()
     }
 	perf_buffer__free(pb);
 	cryptoguard_bpf__destroy(skel);
+
+	CountMinSketch cms;
+    initCMS(&cms);
+
+    processCSVFiles(&cms, "./dataset");
+
+    printCMS(&cms);
+    
+    // Ensure the directory exists
+    struct stat st = {0};
+    if (stat("./dataset/cms", &st) == -1) {
+        mkdir("./dataset/cms", 0700);
+    }
+    
+    saveCMSToCSV(&cms, "./dataset/cms/sketch.csv");
 
 	return -err;
 }
